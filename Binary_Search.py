c2942b4761300a850d8c81a288ab35ef3f3d1972
KarthikHegde <kartikshrikanthegde@gmail.com>
2016-01-20 15:55:29 -0600

    Binary_Search Implemented

--- Binary_Search.py
+++ Binary_Search.py
@@ -16,7 +16,7 @@ def Binary_Search(list, search):
             print "Element",search,"found at position", mid
             break
 
-
+# List of prime numbers
 list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
 		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 search = 67
