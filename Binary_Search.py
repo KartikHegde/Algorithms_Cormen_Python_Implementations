f4d835f7c53fe2102cdc2ef308d46d7a553aba17
KarthikHegde <kartikshrikanthegde@gmail.com>
2016-01-20 15:46:30 -0600

    Unsuccessful search Condition

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,16 +1,24 @@
 def Binary_Search(list, search):
 
-
     min = 0
     max = len(list) - 1
 
-    if max < min or search not in list:
-        print "Unable to search the element. Either the list is empty or search item doesnt exist"
-        return -1
+    if max <= min or search not in list:
+        print "Unable to search the element. List is empty or Element is not present in list"
+
+    while min <= max:
+        mid = (min + max ) / 2
+        if search > list[mid]:
+            min = mid + 1
+        elif search < list[mid]:
+            max = mid -1
+        else:
+            print "Element",search,"found at position", mid
+            break
 
 
+list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+search = 67
 
-list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-search = 60
-
-print Binary_Search(list,search)
+Binary_Search(list,search)
