826591c07824b94bd70994592f01e888876d8fc4
KarthikHegde <karthdz@gmail.com>
2016-02-06 15:48:01 -0600

    Implementation and Analysis of Merge_Sort

--- Merge Sort.py
+++ Merge Sort.py
@@ -17,7 +17,7 @@ recursive calls.
 Lower bound = N log N
 Upper bount = N log N
 
-Hence Merge sort is an optimal Algorithm
+Hence Merge sort is an optimal Algorithm wr.t comparisons but not w.r.t space
 
 '''
 
