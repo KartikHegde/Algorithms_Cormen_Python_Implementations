504d1ddb17f257e5027ba5126106328c0556306b
KarthikHegde <karthdz@gmail.com>
2016-02-16 01:19:32 -0600

    Implementation and Analysis of Quick Sort

--- QuickSort.py
+++ QuickSort.py
@@ -8,43 +8,27 @@ Quick sort cost very less w.r.t space as its an in-place sorting algorithm.
 
 But its not a stable Algorithm w.r.t key pairs sorting. and not suitable for small array size
 
-Running time is : N log N
+Time Complexity : O(n log n)
 
-Best case : N log N
-Worst case : N^2
-Average Case : N log N
+Best case : O(n log n)
+Worst case : O(n**2)
+Average Case : O(n log n)
 
 
 Improve the performance by median finding - and choosing median as pivot is best option
 Finding the median of median sample.
 '''
 
-def merge(list):
-    if len(list) < 2:
-        return list
-    n=len(list)
-    pivot=list[n-1]
-    left=[]
-    right=[]
-    mid=[]
-    for item in list:
-        if item<pivot:
-            left.append(item)
-        elif item>pivot:
-            right.append(item)
-
-    sort(left,right)
-    mid.append(pivot)
-    result=left+mid+right
-    return result
-
-def sort(left,right):
-
-    new_left=left.sort()
-    new_right=right.sort()
-    return new_left,new_right
+import random
+def qsort(L):
+    if len(L)<2: return L
+    pivot_element = random.choice(L)
+    small = [i for i in L if i< pivot_element]
+    medium = [i for i in L if i==pivot_element]
+    large = [i for i in L if i> pivot_element]
+    return qsort(small) + medium + qsort(large)
 
 
-list=[9,7,5,11,12,2,14,3,10,6]
-print "List before sorting", list
-print "List after sorting",merge(list)
+my_list = [3,2,6,9,4,1,8]
+print "List before sort",my_list
+print "List after Sort",qsort(my_list)
